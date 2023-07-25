f113f1cb83f0e9026704b59696daba8fb0532161
Rahul Chouhan <rchouhan791@gmail.com>
2023-07-26 03:18:01 +0530

    reading data in chunks

--- initialize_db.py
+++ initialize_db.py
@@ -11,10 +11,17 @@ def initialize_database():
     cursor = connection.cursor()
 
     cursor.execute("DROP TABLE IF EXISTS VINAUDIT;")
-    
-    df = pd.read_csv("database/NEWTEST-inventory-listing-2022-08-17.txt", sep='|')
 
-    # print(df.columns)
+    csv_file = 'database/NEWTEST-inventory-listing-2022-08-17.txt'
+
+    chunk_size = 100000
+    chunks_list = []
+
+    #Reading data in chunks
+    for chunk in pd.read_csv(csv_file, sep='|', chunksize=chunk_size):
+        chunks_list.append(chunk)
+    
+    df = pd.concat(chunks_list)
 
     df["vehicle"] = df['year'].astype(str) + " " + df['make'].astype(str) + " " + df['model'].astype(str)
 
@@ -22,6 +29,7 @@ def initialize_database():
 
     df.to_sql('VINAUDIT', connection, index=False)
 
+    #Creating indes on column 'vehicle' for faster queries 
     cursor.execute("CREATE INDEX idx_vehicle ON VINAUDIT (vehicle);")
 
     print("Inventory data dumped to table VINAUDIT")
